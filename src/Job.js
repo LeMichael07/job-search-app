ace96b4324d3cb4c2ab1ac48e4aa3d4ba5e717a8
LeMichael07 <lemichael07@gmail.com>
2019-09-30 18:34:43 -0400

    first commit

--- src/Job.js
+++ src/Job.js
@@ -21,7 +21,7 @@ class Job extends Component {
     return (
       <div className="Job">
         <h1>{this.state.title}</h1>
-        <a href="{this.state.company_url}"><img src="{this.state.company_logo}" width="100"/></a>
+        <a href="{this.state.company_url}"><img src={this.state.company_logo} width="100"/></a>
         <div dangerouslySetInnerHTML={{__html: this.state.how_to_apply}}></div>
         <span dangerouslySetInnerHTML={{__html: this.state.description}}></span>
       </div>
